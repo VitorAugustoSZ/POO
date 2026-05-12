a98918c33306fbe73286ef43fb75fc4224e7250b
vitoraugustosz <vitoraugusto.szslv@gmail.com>
2026-05-12 20:23:03 -0300

    Aula de visualização gráfica com java

---
+++ correcaoProva/src/main/java/ads/poo/Main.java
@@ -0,0 +1,53 @@
+package ads.poo;
+
+import edu.princeton.cs.algs4.Draw;
+
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() throws InterruptedException {
+
+        Draw draw = new Draw();
+        ;
+        /*
+        draw.circle(400,400,100);
+        draw.setPenColor(Color.RED);
+        draw.filledCircle(400, 400, 50);
+          */
+
+        draw.setCanvasSize(1000,1000);
+        draw.setXscale(0,1000);
+        draw.setYscale(0,1000);
+        draw.enableDoubleBuffering();
+        /*
+        for (int i = 0; i < 10; i++) {
+            draw.text(500,500,"" + i);
+            draw.show();
+            TimeUnit.SECONDS.sleep(1);
+            draw.clear();
+        }
+
+         */
+
+
+
+        draw.line(100, 800, 300, 890);
+        draw.line(900, 800, 700, 890);
+
+        draw.circle(500,1348,500);
+        draw.setPenColor(Color.white);
+        draw.filledRectangle(500,1040, 500, 150);
+        /*
+        double[] x = {150,250,300,100};
+        double[] y = {150,150,100,100};
+        draw.polygon(x,y);
+*/
+        draw.show();
+
+
+
+    }
+}
